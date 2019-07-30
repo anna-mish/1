932d1c7e8ac7bffa4d93f061a4bb8d7d92386770
znova.roman <roman.znova@omnigon.com>
2019-07-30 09:15:46 +0300

    update tabs logic on click

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,21 +1,19 @@
 import $ from './libs/jquery';
 
 const onPageLoaded = () => {
+    $('#content > div').hide(); // Скрываем содержание
+    $('#tabs li:first').addClass('current'); // Активируем первую закладку
+    $('#content div:first').fadeIn(); // Выводим содержание
 
+    $('#tabs a').on('click', function(e) {
+        e.preventDefault();
+        $('#content > div').hide(); //Скрыть все сожержание
+        $('#tabs li').removeClass('current'); //Сброс ID
+        $(this).parent().addClass('current'); // Активируем закладку
+        $('#' + $(this).attr('title')).fadeIn(); // Выводим содержание текущей закладки
+    });
+
+    $('#tabs li:first').click();
 };
 
 $(document).ready(onPageLoaded);
-
-$(document).ready(function() {
-	$("#content div").hide(); // Скрываем содержание
-	$("#tabs li:first").attr("id","current"); // Активируем первую закладку
-	$("#content div:first").fadeIn(); // Выводим содержание
-    
-    $('#tabs a').click(function(e) {
-        e.preventDefault();        
-        $("#content div").hide(); //Скрыть все сожержание
-        $("#tabs li").attr("id",""); //Сброс ID
-        $(this).parent().attr("id","current"); // Активируем закладку
-        $('#' + $(this).attr('title')).fadeIn(); // Выводим содержание текущей закладки
-    });
-});
