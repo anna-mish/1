15f1e1d5fd3d1fb2408ade356de3c46a68cf85c0
anna-mish <anna-mish@users.noreply.github.com>
2019-08-07 14:59:39 +0300

    rename id

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,19 +1,19 @@
 import $ from './libs/jquery';
 
 const onPageLoaded = () => {
-    $('#content > div').hide(); // Скрываем содержание
-    $('#tabs li:first').addClass('current'); // Активируем первую закладку
-    $('#content div:first').fadeIn(); // Выводим содержание
+    $('#user-content > div').hide(); // Скрываем содержание
+    $('#user-tabs li:first').addClass('current'); // Активируем первую закладку
+    $('#user-content div:first').fadeIn(); // Выводим содержание
 
-    $('#tabs a').on('click', function(e) {
+    $('#user-tabs a').on('click', function(e) {
         e.preventDefault();
-        $('#content > div').hide(); //Скрыть все сожержание
-        $('#tabs li').removeClass('current'); //Сброс ID
+        $('#user-content > div').hide(); //Скрыть все сожержание
+        $('#user-tabs li').removeClass('current'); //Сброс ID
         $(this).parent().addClass('current'); // Активируем закладку
         $('#' + $(this).attr('title')).fadeIn(); // Выводим содержание текущей закладки
     });
 
-    $('#tabs li:first').click();
+    $('#user-tabs li:first').click();
 };
 
 $(document).ready(onPageLoaded);
