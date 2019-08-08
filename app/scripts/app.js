c17c8fdfb03b902ddce4962e112b7285cc9cbc56
anna-mish <anna-mish@users.noreply.github.com>
2019-08-08 10:21:11 +0300

    updade js tabs

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,21 +1,6 @@
 import $ from './libs/jquery';
 
-const onPageLoaded = () => {
-
-    $('#user-content > div').hide(); // Скрываем содержание
-    $('#user-tabs li:first').addClass('current'); // Активируем первую закладку
-    $('#user-content div:first').fadeIn(); // Выводим содержание
-
-    $('#user-tabs a').on('click', function(e) {
-        e.preventDefault();
-        $('#user-content > div').hide(); //Скрыть все сожержание
-        $('#user-tabs li').removeClass('current'); //Сброс ID
-        $(this).parent().addClass('current'); // Активируем закладку
-        $('#' + $(this).attr('title')).fadeIn(); // Выводим содержание текущей закладки
-    });
-
-    $('#user-tabs li:first').click();
-
+const onPageLoaded = () => { 
     function initTabs(mainContainer, tabsContainer) {
         const CONTENT_ITEM = `${mainContainer} >  div`;
         const CONTENT_ITEM_FIRST = `${mainContainer} >  div:first`;
@@ -39,6 +24,8 @@ const onPageLoaded = () => {
     };
 
     initTabs('#content','#tabs');
+    initTabs('#user-content','#user-tabs');
+
 
 };
 
